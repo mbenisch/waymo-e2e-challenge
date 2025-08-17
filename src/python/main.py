4437cfa725949d336df26f364e05c8a3ade8cd67
Michael Benisch <mbenisch@gmail.com>
2025-08-17 10:44:12 -0400

    Update main.py file for testing

--- src/python/main.py
+++ src/python/main.py
@@ -1,14 +1,15 @@
-import torch
 import tensorflow as tf
 import pdb
-from src.python.waymo_open_dataset.protos import vector_pb2, map_pb2
+from src.python.waymo_open_dataset.protos import dataset_pb2 as open_dataset
 
-# Specify the path to your TFRecord file(s)
-tfrecord_path = '/Users/mbenisch/Downloads/test_202504211836-202504220845.tfrecord-00000-of-00266'
+from src.python.waymo_open_dataset.protos import end_to_end_driving_data_pb2 as wod_e2ed_pb2
+from src.python.waymo_open_dataset.protos import end_to_end_driving_submission_pb2 as wod_e2ed_submission_pb2
+
+
+tfrecord_path = '/Users/mbenisch/Downloads/training_202504031202_202504151040.tfrecord-00000-of-00263'
 dataset = tf.data.TFRecordDataset(tfrecord_path)
+dataset_iter = dataset.as_numpy_iterator()
+bytes_example = next(dataset_iter)
+data = wod_e2ed_pb2.E2EDFrame()
+data.ParseFromString(bytes_example)
 pdb.set_trace()
-
-for record in dataset:
-    example = tf.train.Example.FromString(record)
-    print(example)
-    break
